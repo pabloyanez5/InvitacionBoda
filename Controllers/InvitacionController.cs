3a37a50939162258e6e3a087d30b2e72a5b88780
pabloyanez5 <130101518+pabloyanez5@users.noreply.github.com>
2025-06-04 17:03:47 -0500

    UPLOAD

---
+++ Controllers/InvitacionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TuProyecto.Controllers
+{
+    public class InvitacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Confirmacion()
+        {
+            return View(); // Carga Confirmacion.cshtml
+        }
+
+        public IActionResult Confirmado()
+        {
+            return View(); // Carga Confirmado.cshtml
+        }
+    }
+}
